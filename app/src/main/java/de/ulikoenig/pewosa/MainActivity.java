85d020d976859f3dd75629ef1346a084c5b1c84a
ulikoenig <github@bills-erben.de>
2016-09-25 23:01:38 +0200

    Initial commit

---
+++ app/src/main/java/de/ulikoenig/pewosa/MainActivity.java
@@ -0,0 +1,245 @@
+package de.ulikoenig.pewosa;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import static de.ulikoenig.pewosa.R.id.action_settings;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String PEWOSA_BASE_URL = "https://ulikoenig.de/pewosa/";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private Toolbar mToolbar;
+    public static final String PREFS_NAME = "de.ulikoenig.pewosa.settings";
+
+    private String username;
+    private String password;
+    private WebView webview;
+    private TextView textView;
+    private MenuItem settingsbtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("pewosa", "onCreate");
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        username = settings.getString("username", "");
+        password = settings.getString("password", "");
+
+        setContentView(R.layout.activity_main);
+        webview = (WebView) findViewById(R.id.webView);
+        textView = (TextView) findViewById(R.id.textView);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setUserAgentString("de.ulikoenig.pewosa/android");
+        webview.clearHistory();
+
+        //Webseite in Browser laden
+        webview.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+                Log.d("pewosa", "Splash Webview finished!");
+                if (textView != null) textView.setVisibility(View.INVISIBLE);
+                if (settingsbtn != null) {
+                    settingsbtn.setEnabled(true);
+                    settingsbtn.setVisible(true);
+                }
+                if (webview != null) {
+                    webview.setVisibility(View.VISIBLE);
+                    webview.setEnabled(true);
+                    webview.setAlpha(1f);
+                    //stelle History auf Null, wenn Startseite geladen.
+                    if (url.equals(PEWOSA_BASE_URL)) {
+                        webview.clearHistory();
+                    }
+                }
+            }
+
+            public void onPageStarted(WebView view,
+                                      String url,
+                                      Bitmap favicon) {
+                waitForWebview(view);
+            }
+        });
+
+//        webview.loadUrl(PEWOSA_BASE_URL + "login.php?login=1&username=" + username + "&password=" + password);
+
+        if ((savedInstanceState == null) || (!savedInstanceState.containsKey("NotificationMessageType"))) {
+            webview.loadUrl(PEWOSA_BASE_URL + "login.php?login=1&username=" + username + "&password=" + password);
+        } else {
+            onNewIntent(getIntent());
+        }
+
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        FirebaseMessaging.getInstance().subscribeToTopic("user" + username);
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
+
+
+        String token = FirebaseInstanceId.getInstance().getToken();
+        // Log and toast
+
+        if (token != null) {
+            Log.d("firebase", token.toString());
+        } else {
+            Log.d("firebase", "token==NULL!");
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadHome(v);
+            }
+        });
+    }
+
+
+    /**
+     * catch Notification todos
+     *
+     * @param intent
+     */
+    @Override
+    public void onNewIntent(Intent intent) {
+        Log.d("pewosa", "onNewIntent");
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            Log.d("pewosa", "onNewIntent - found extra data");
+            if (extras.containsKey("NotificationMessageType")) {
+                Log.d("pewosa", "onNewIntent - found NotificationMessageType");
+
+                if (extras.getString("NotificationMessageType").equals("newFirstReleaseRequest")) {
+                    int id = extras.getInt("NotificationMessageID");
+                    Log.d("pewosa", "onNewIntent - found newFirstReleaseRequest " + id);
+                    webview.loadUrl(PEWOSA_BASE_URL + "message.php#" + id);
+                }
+
+                if (extras.getString("NotificationMessageType").equals("newSecondReleaseRequest")) {
+                    int id = extras.getInt("NotificationMessageID");
+                    Log.d("pewosa", "onNewIntent - found newSecondReleaseRequest " + id);
+                    webview.loadUrl(PEWOSA_BASE_URL + "message.php#" + id);
+                }
+            }
+        } else {
+            Log.d("pewosa", "onNewIntent extras == null");
+        }
+  }
+
+
+    public void loadHome(View v) {
+        Log.d("pewosa", "loadHome");
+        WebView webview = (WebView) findViewById(R.id.webView);
+        waitForWebview(webview);
+        webview.loadUrl(PEWOSA_BASE_URL + "login.php?login=1&username=ukoenig&password=hugo");
+    }
+
+    private void waitForWebview(WebView webview) {
+        if (webview != null) {
+            webview.setAlpha(0.25f);
+            webview.setEnabled(false);
+        }
+        if (settingsbtn != null) {
+            settingsbtn.setEnabled(false);
+            settingsbtn.setVisible(false);
+        }
+    }
+
+
+    public void reloadWebview(MenuItem item) {
+        Log.d("pewosa", "reloadWebview");
+        waitForWebview(webview);
+        webview.reload();
+    }
+
+    public void openSettings(MenuItem item) {
+        Log.d("pewosa", "openSettings");
+        setContentView(R.layout.activity_login);
+        Intent i = new Intent(getBaseContext(), LoginActivity.class);
+        this.startActivity(i);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if ((webview != null) && (webview.canGoBack())) {
+            waitForWebview(webview);
+            webview.goBack();
+            return;
+        }
+
+//        android.os.Process.killProcess(android.os.Process.myPid());
+//        System.exit(1);
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_MAIN);
+        i.addCategory(Intent.CATEGORY_HOME);
+        this.startActivity(i);
+        //super.onBackPressed();
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        settingsbtn = menu.findItem(R.id.action_settings);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/de/ulikoenig/pewosa/MyFirebaseMessagingService.java
@@ -0,0 +1,194 @@
+package de.ulikoenig.pewosa;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // [START_EXCLUDE]
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        // [END_EXCLUDE]
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+
+        if ((remoteMessage.getData() != null) && (remoteMessage.getData().get("msgtype") != null)) {
+
+            if (remoteMessage.getData().get("msgtype").equals("newFirstReleaseRequest")) {
+                int id = extractID(remoteMessage);
+                String title = remoteMessage.getData().get("pmTitle");
+                if ((id > 0) && (title != null)) {
+                    newFirstReleaseRequest(title, id);
+                }
+            }
+            if (remoteMessage.getData().get("msgtype").equals("removeFirstReleaseRequest")) {
+                int id = extractID(remoteMessage);
+                if (id > 0) {
+                    removeReleaseRequest(id);
+                }
+            }
+
+            //Ids von 2. Freigabe *(-1) um Racecondition beim löschen der alten notification zu vermeiden.
+
+            if (remoteMessage.getData().get("msgtype").equals("newSecondReleaseRequest")) {
+                int id = extractID(remoteMessage);
+                String title = remoteMessage.getData().get("pmTitle");
+                String firstReleaseingUsername = remoteMessage.getData().get("firstReleaseingUsername");
+                if ((id > 0) && (title != null) && (firstReleaseingUsername != null)) {
+                    newSecondReleaseRequest(title, id, firstReleaseingUsername);
+                }
+            }
+            if (remoteMessage.getData().get("msgtype").equals("removeSecondReleaseRequest")) {
+                int id = Integer.parseInt(remoteMessage.getData().get("id"));
+                removeReleaseRequest(id*(-1));
+            }
+        }
+
+
+    }
+
+    private int extractID(RemoteMessage remoteMessage) {
+        String sid = remoteMessage.getData().get("id");
+        int id = 0;
+        if (sid != null) {
+            id = Integer.parseInt(sid);
+        }
+        return id;
+    }
+    // [END receive_message]
+
+
+//    /**
+//     * Create and show a simple notification containing the received FCM message.
+//     *
+//     * @param messageBody FCM message body received.
+//     */
+//    private void sendNotification(String messageBody) {
+//        Intent intent = new Intent(this, MainActivity.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+//                PendingIntent.FLAG_ONE_SHOT);
+//
+//        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+//                .setSmallIcon(R.drawable.ic_stat_ic_notification)
+//                .setContentTitle("FCM Message")
+//                .setContentText(messageBody)
+//                .setAutoCancel(true)
+//                .setSound(defaultSoundUri)
+//                .setContentIntent(pendingIntent);
+//
+//        NotificationManager notificationManager =
+//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//
+//        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+//    }
+
+
+
+
+
+
+    private void newFirstReleaseRequest(String titel, int id) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("NotificationMessageType", "newFirstReleaseRequest");
+        intent.putExtra("NotificationMessageID", id);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, id, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        long[] vibrate = {0, 300, 1000,150,1000,300 };
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                .setContentTitle("Bitte um 1. Freigabe:")
+                .setContentText(titel)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+                .setVibrate(vibrate);
+
+
+//        Intent notificationIntent = new Intent(this, MainActivity.class);
+//        PendingIntent contentIntent = PendingIntent.getActivity(this, id, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        notificationBuilder.setContentIntent(contentIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(id, notificationBuilder.build());
+    }
+
+
+    private void newSecondReleaseRequest(String titel, int id, String firstReleaseingUsername) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("NotificationMessageType", "newSecondReleaseRequest");
+        intent.putExtra("NotificationMessageID", id);
+        intent.putExtra("firstReleaseingUsername", firstReleaseingUsername);
+        long[] vibrate = {0, 150, 1000,300,1000,150 };
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, id*(-1), intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                .setContentTitle("Bitte um 2. Freigabe:")
+                .setContentText(titel)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+                .setVibrate(vibrate);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(id*(-1), notificationBuilder.build());
+    }
+
+    private void removeReleaseRequest(int id) {
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(id);
+    }
+
+
+}
